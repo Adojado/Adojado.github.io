75ac1b27fc926d16e905db126ef92a5116f06637
Adrian Biedrzycki <adrian.biedrzycki@gate-software>
2023-01-19 16:38:26 +0100

    languages 2

--- poems/languages/js.js
+++ poems/languages/js.js
@@ -2,8 +2,4 @@ $(document).ready(function() {
     setTimeout(function () {
         $('.container').toggleClass('animate');
     }, 2000)
-
-    setTimeout(function () {
-        $('.sorry').html('это должно было быть похоже на сердце, но я не могу(((');
-    }, 8000)
 });
